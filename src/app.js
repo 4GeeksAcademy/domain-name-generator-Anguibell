50a3dfc72d65e26cf37d10d2bd6db010a037afc2
Anguibell <romeroanguibell17@gmail.com>
2024-09-08 02:24:06 +0000

    Modified

--- src/app.js
+++ src/app.js
@@ -5,31 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let extensions = [".es", ".com", ".net", ".us", ".io"];
-let domainHacks = {
-  ".es": "es",
-  ".com": "com",
-  ".net": "net",
-  ".us": "us",
-  ".io": "io"
-};
+const PRONOUNS = ["the", "our"];
+const ADJECTIVES = ["great", "big"];
+const NOUNS = ["jogger", "racoon"];
+const EXTENSIONS = [".es", ".com", ".net", ".us", ".io"];
 
-function generarDominio() {
-  // Seleccionamos un elemento aleatorio de cada arreglo
-  let pronounRandom = pronoun[Math.floor(Math.random() * pronoun.length)];
-  let adjRandom = adj[Math.floor(Math.random() * adj.length)];
-  let nounRandom = noun[Math.floor(Math.random() * noun.length)];
-  let extensionsRandom =
-    extensions[Math.floor(Math.random() * extensions.length)];
+function generateDomain() {
+  const randomPronoun = getRandomElement(PRONOUNS);
+  const randomAdjective = getRandomElement(ADJECTIVES);
+  const randomNoun = getRandomElement(NOUNS);
+  const randomExtension = getRandomElement(EXTENSIONS);
 
-  let domain = `${pronounRandom}${adjRandom}${nounRandom}${extensionsRandom}`;
-  return domain;
+  return `${randomPronoun}${randomAdjective}${randomNoun}${randomExtension}`;
+}
+
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 window.onload = function() {
-  //write your code here
-  console.log(generarDominio());
+  console.log(generateDomain());
 };
